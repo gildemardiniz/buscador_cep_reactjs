8453b06b6c2ef965eb32fa1d1c3cd92ee284a94a
Gildemar <gildemardiniz@gmail.com>
2023-06-23 17:38:27 -0300

    primiero commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import './app.css';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
 
 function App() {
+
+  const [endereco, setEndereco] = useState({});
+
+
+  function capturandoCep(evento){
+
+    const cep = evento.target.value
+
+    setEndereco({
+      cep
+    })
+
+    if(cep && cep.length === 8){
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(dados => {
+        setEndereco({
+          cep: dados.cep,
+          rua: dados.logradouro,
+          bairro: dados.bairro,
+          cidade: dados.localidade,
+          uf: dados.uf,
+          ddd: dados.ddd,
+        })
+      })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-section">
+        <form className="App-section-form" onSubmit={evento => evento.preventDefault()}>
+          <h2>Buscador de CEP</h2>
+         <div className='Busca-cep'>
+         <TextField className="App-section-form-textfield"
+            id="outlined-basic" 
+            label="Digite o CEP" 
+            variant="outlined"
+            onChange={capturandoCep}
+            color="primary"
+            size='small'
+            margin="dense"
+          />
+         </div>
+          <div>
+            <TextField
+              size="small"
+              margin="dense"
+              value={endereco.rua}
+              id="outlined-start-adornment"
+              sx={{ m: 1, width: '98%'}}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">Rua</InputAdornment>,
+              }}
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              size="small"
+              margin="dense"
+              value={endereco.bairro}
+              id="outlined-start-adornment"
+              sx={{ m: 1}}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">Bairro</InputAdornment>,
+              }}
+              variant="outlined"
+            />
+            <TextField
+              size="small"
+              margin="dense"
+              value={endereco.cidade}
+              id="outlined-start-adornment"
+              sx={{ m: 1}}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">Cidade</InputAdornment>,
+              }}
+              variant="outlined"
+            />
+            <TextField
+              size="small"
+              margin="dense"
+              value={endereco.uf}
+              id="outlined-start-adornment"
+              sx={{ m: 1}}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">UF</InputAdornment>,
+              }}
+              variant="outlined"
+            />
+          </div>
+        </form>
+      </section>
     </div>
-  );
+);
 }
 
 export default App;
